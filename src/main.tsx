5ebe80c4adc9a0002e35fb622a3cd1aa1cd315e4
lhj21025-beep <lhj21025@gmail.com>
2026-09-16 15:08:07 +0900

    Register PWA service worker

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from './App'
 import AuthGate from './AuthGate'
 import './styles.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('./sw.js').catch(console.error))
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthGate>
